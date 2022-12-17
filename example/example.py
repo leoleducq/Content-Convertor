00c599ecfe1e86d0ddeb63a8281fcad3011eb7ac
leoleducq <73238856+leoleducq@users.noreply.github.com>
2022-12-17 12:51:24 +0100

    Update example.py

--- example/example.py
+++ example/example.py
@@ -3,34 +3,23 @@ import os
 
 import contentconvertor.contentconvertor as cc
 
+# Récupère les crédentials dans le fichier credentials.json
+with open("credentials.json", "r") as f :
+    credentials = json.load(f)
+BEARER_TOKEN = credentials["BEARER_TOKEN"]
 
-class contentconvertor :
-
-    def __init__(self, CONSUMER_KEY : str, CONSUMER_SECRET : str, ACCESS_TOKEN : str, ACCESS_TOKEN_SECRET : str, user : str, nb_tweets : int) :
-        api = cc.connexion_to_api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        self.tweets = cc.get_tweets(api ,user, nb_tweets=nb_tweets)
-        self.user = user
-        self.main()
-
-    def main(self) :
-        """Fonction principale"""
-        # Récupère les tweets de l'utilisateur
-        user = self.user
-        tweets = self.tweets
-        # Télécharge les tweets
-        cc.download_tweets(user, tweets)
-        #Liste des images précedemment téléchargées
-        list_img = [f for f in os.listdir('.') if f.endswith('.png')]
-        for path in list_img :
-            img = cc.get_image(path)
-            cc.resizing(img, path)
-
-if __name__ == "__main__":
-    # Récupère les crédentials dans le fichier credentials.json
-    with open("credentials.json", "r") as f :
-        credentials = json.load(f)
-    CONSUMER_KEY = credentials["CONSUMER_KEY"]
-    CONSUMER_SECRET = credentials["CONSUMER_SECRET"]
-    ACCESS_TOKEN = credentials["ACCESS_TOKEN"]
-    ACCESS_TOKEN_SECRET = credentials["ACCESS_TOKEN_SECRET"]
-    contentconvertor(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, input("User : "), input("Nombre de tweets : "))
+# Récupère la connexion à l'API
+api = cc.connexion_to_api(BEARER_TOKEN)
+# Récupère l'id de l'utilisateur
+user_id = cc.get_user_id(api, "iziatask")
+# Récupère les tweets de l'utilisateur
+tweets = cc.get_tweets(api, user_id, exclude=["replies", "retweets"])
+# Télécharge les tweets
+cc.download_tweets(user_id, tweets)
+#Liste des images précedemment téléchargées
+list_img = [f for f in os.listdir('.') if f.endswith('.png')]
+for path in list_img :
+    # Récupère l'image
+    img = cc.get_image(path)
+    # Redimensionne l'image
+    cc.resizing(img, path)
