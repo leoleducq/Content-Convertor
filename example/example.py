9740278678c3f981d86b293910a078f18ba90627
leoleducq <73238856+leoleducq@users.noreply.github.com>
2022-12-21 01:33:11 +0100

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,18 +1,16 @@
 import json
 
-import contentconvertor.contentconvertor as cc
-
+from contentconvertor import tweet2gram as t2g
 # Récupère les crédentials dans le fichier credentials.json
 with open("credentials.json", "r") as f :
     credentials = json.load(f)
 BEARER_TOKEN = credentials["BEARER_TOKEN"]
-
 # Récupère la connexion à l'API
-api = cc.connexion_to_api(BEARER_TOKEN)
+api = t2g.connexion_to_api(BEARER_TOKEN)
 # Récupère l'id de l'utilisateur
-user_id = cc.get_user_id(api, "iziatask")
+user_id = t2g.get_user_id(api, "iziatask")
 # Récupère les tweets de l'utilisateur
-tweets = cc.get_tweets(api, user_id, exclude=["replies", "retweets"])
+tweets = t2g.get_tweets(api, user_id, exclude=["replies", "retweets"])
 for tweet in tweets.data :
     # Télécharge les tweets
-    cc.download_tweet(user_id, tweet)
+    t2g.download_tweet(user_id, tweet)
